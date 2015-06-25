cafe8a03cb35b3d53e389e0171f8e7335b6b8f55
Ross Robert McKee <rossrobertmckee@Rosss-MacBook-Air.local>
2015-06-25 12:22:32 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Acmetravel::Application.config.secret_key_base = '0f5bcdad231b526f7a61e8822f9f7b98fd7405237a541d75e85d042e0fab02fbdd83217a09e1beec94144de714835c479711e773e8524aa3a78e7e1fd3e23e19'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Acmetravel::Application.config.session_store :cookie_store, key: '_acmetravel_session'
