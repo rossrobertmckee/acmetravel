d97e85775c160b485f876dba6df526d9aa62a5d2
Ross Robert McKee <rossrobertmckee@gmail.com>
2015-08-06 15:14:46 -0400

    Airport data loaded via model

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,40 +1,41 @@
 class Airport < ActiveRecord::Base
 	validates :code, uniqueness: true
-	attr_accessor :code, :lat, :lon, :name, :city, :state, :country, :woeid,
-                :tz, :phone, :email, :url, :runway_length, :elev, :icao,
-                :direct_flights, :carriers
 
 	def self.build_from_json(raw_data) 
-		self.code = raw_data["code"]
-		self.lat = raw_data["lat"]
-		self.lon = raw_data["lon"]
-		self.name = raw_data["name"]
-		self.city = raw_data["city"]
-		self.state = raw_data["state"]
-		self.country = raw_data["country"]
-		self.woeid = raw_data["woeid"]
-    self.tz = raw_data["tz"]
-    self.phone = raw_data["phone"]
-    self.email = raw_data["email"]
-    self.url = raw_data["url"]
-    self.runway_length = raw_data["runway_length"]
-    self.elev = raw_data["elev"]
-    self.icao = raw_data["icao"]
-    self.direct_flights = raw_data["direct_flights"]
-    self.carriers = raw_data["carriers"]
+		a = Airport.new
+		a.code = raw_data["code"]
+		a.lat = raw_data["lat"]
+		a.lon = raw_data["lon"]
+		a.name = raw_data["name"]
+		a.city = raw_data["city"]
+		a.state = raw_data["state"]
+		a.country = raw_data["country"]
+		a.woeid = raw_data["woeid"]
+    a.tz = raw_data["tz"]
+    a.phone = raw_data["phone"]
+    a.email = raw_data["email"]
+    a.url = raw_data["url"]
+    a.runway_length = raw_data["runway_length"]
+    a.elev = raw_data["elev"]
+    a.icao = raw_data["icao"]
+    a.direct_flights = raw_data["direct_flights"]
+    a.carriers = raw_data["carriers"]
+    return a
 	end
 	
 	def self.load_data
 		airport_data = JSON.parse(File.read('config/data/airport_data.json'))
-			airport_data.map do |breakdown_data|
-				self.build_from_json(breakdown_data)
+			airport_data.each do |breakdown_data|
+				breakdown_data.delete("type")
+				airport_data_variable = self.build_from_json(breakdown_data)
+				airport_data_variable.save
 			end
 		end
 
 	
 	# validates :lat, uniqueness: true
 	# validates :lon, uniqueness: true
-	validates :name, uniqueness: true
+	# validates :name, uniqueness: true
 	# validates :city, uniqueness: true
 	# validates :state, uniqueness: true
 	# validates :country, uniqueness: true
@@ -46,7 +47,7 @@ class Airport < ActiveRecord::Base
 	# validates :url, uniqueness: true
 	# validates :runway_length, uniqueness: true
 	# validates :elev, uniqueness: true
-	validates :icao, uniqueness: true
+	# validates :icao, uniqueness: true
 	# validates :direct_flights, uniqueness: true
 	# validates :carriers, uniqueness: true
 
