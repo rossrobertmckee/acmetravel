a4c12d03c65dffc1c98836306e497088771f882d
Ross Robert McKee <rossrobertmckee@gmail.com>
2015-09-02 16:38:50 -0400

    Validations and flash completed for search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -8,12 +8,16 @@ class FlightsController < ApplicationController
 	end
 
   def search
+    maxfare = (params[:search])[:maxfare]
     response = Api::Sabre::Latest.flights(params[:search])
     if response["status"] == "NotProcessed"
-      flash[:warning] =  "It worked 1"
+      flash[:warning] =  "Something you entered was not valid. Maybe the airport code?"
+      redirect_to root_path
+    elsif response["status"] == "Complete" && ((maxfare.to_i) < 200)
+      flash[:warning] =  "No results were found! Double check the airport code, or maybe try forking out a little more cash?"
       redirect_to root_path
     elsif response["status"] == "Complete"
-      flash[:warning] =  "It worked 2"
+      flash[:warning] =  "No results were found, please try again."
       redirect_to root_path
     else
       @flight_data = FlightData.new(JSON.parse(response.body)["FareInfo"])
